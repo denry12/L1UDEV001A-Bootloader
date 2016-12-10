2de1e27d739e66a7c7f4f925a6b8ef196642e004
denry12 <hopkinsfbi@hotmail.com>
2016-12-10 21:34:50 +0200

    wrote basic flow of bootloader
    Basically ignores everything at the moment, but every step is explained.
    Just needs doing.

--- src/L1UDEV001A-Bootloader.c
+++ src/L1UDEV001A-Bootloader.c
@@ -6,3 +6,87 @@
  */
 
 
+//these are here for informative purposes only atm, should be written into jumpToApplication function
+#define APP_INFO_ADDR_START 0x00001000
+#define APP_SP_ADDR 0x00001100
+#define APP_ADDR_START 0x00001104
+
+void jumpToApplication(){
+	//app stack pointer initial value to stack pointer
+	asm volatile("ldr r0, =0x1100");
+	asm volatile("ldr r0, [r0]");
+	asm volatile("mov sp, r0");
+
+	//app reset vector address to program counter
+	asm volatile("ldr r0, =0x1104");
+	asm volatile("ldr r0, [r0]");
+	asm volatile("mov pc, r0");
+
+	return; //o no, we shouldn't get here
+}
+
+int isExtFlashInstalled(){
+
+	return 0; //no external flash, too bad.
+	return 1; //totally there
+}
+
+int getExtFlashVersion(){
+	int version=0;
+
+	return version;
+}
+
+int getIntFlashVersion(){
+	int version=0;
+
+	return version;
+}
+
+int isExtFlashChecksumOK(){
+	//verify flags first, that is faster
+
+	//verify checksum
+
+	return 0; //very broken
+	return 1; //is OK
+}
+
+int getMCUID(){
+	int ID;
+
+	ID=35; //(LPC11U)35, 64kB flash
+	return ID;
+}
+
+int isIntFlashChecksumOK(){
+	if(getMCUID == 35){
+		//check for LPC11U35
+	} else return 1; //Others not supported yet, so, yeah, sure why not.
+
+	return 1; //is OK
+	return 0; //very broken
+
+}
+
+
+
+int main(){
+	if(isExtFlashInstalled()){ //aww yeah we have flash, time to do things
+		if(getExtFlashVersion!=getIntFlashVersion){ //time to update internal flash
+			if(isExtFlashChecksumOK()){ //image seems OK
+				//update internal flash! Make sure to do it according to model
+			}
+		}
+	}
+
+	if(isIntFlashChecksumOK()) jumpToApplication();
+
+	//attempt flashing stock image
+	if(isIntFlashChecksumOK()) jumpToApplication();
+
+	while(1){ //lock MCU
+		//indicate major error?
+	}
+	return 1; //o no!?
+}
